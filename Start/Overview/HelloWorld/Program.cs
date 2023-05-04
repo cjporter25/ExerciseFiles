978683851b8b34e676745f113e6c026183b7c05f
cjporter25 <chris.j.porter25@gmail.com>
2023-05-03 20:02:15 -0500

    Beginning of C# Overview - LinkedIn Learning

--- Start/Overview/HelloWorld/Program.cs
+++ Start/Overview/HelloWorld/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+//namespace HelloWorld
+//{
+//  class Program
+//  {
+//    static void Main(string[] args)
+//    {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your Name?");
+            string str = Console.ReadLine();
+            Console.WriteLine("Why, hello there " + str);
+//     }
+//  }
+//}
+
+
