827534cec74bd6b09f31bc0a5d3e605af3f5f20d
cjporter25 <chris.j.porter25@gmail.com>
2023-05-06 16:37:33 -0500

    Completed Conditionals with "switch" - 05_06_23

--- Start/Program Flow/Conditional-switch/Program.cs
+++ Start/Program Flow/Conditional-switch/Program.cs
@@ -6,9 +6,33 @@ namespace ConditionalOps
     {
         static void Main(string[] args)
         {
-            int theVal = 50;
+            int theVal = 53;
 
-            // TODO: The switch statement
+        // TODO: The switch statement
+            // The value in the switch statement is the thing to evaluate
+            switch (theVal) {
+            // Establishes test cases to compare to the switch value
+            case 50:
+                Console.WriteLine("theVal is 50");
+                // Break if a case was successfully executed. This prevents further
+                //  cases from being checked
+                break;
+            case 51:
+                Console.WriteLine("theVal is 51");
+                break;
+            // Can establish multiple cases in a row and have a final concluding
+            //  execution occur based on the previous cases
+            case 52:
+            case 53:
+            case 54:
+                Console.WriteLine("theVal is between 52 and 54");
+                break;
+            // The default case occurs if none of the previous cases satisfied the
+            //  compared to value
+            default:
+                Console.WriteLine("theVal is something else");
+                break;
+            }
 
         }
     }
