3cf3f1c5e5b51441d7e8f878424909fd956f51be
cjporter25 <chris.j.porter25@gmail.com>
2023-05-06 21:28:49 -0500

    Completed Break and Continue - 05_06_23

--- Start/Program Flow/BreakContinue/Program.cs
+++ Start/Program Flow/BreakContinue/Program.cs
@@ -11,14 +11,25 @@ namespace BreakContinue
             Console.WriteLine("Using break and continue :");
             foreach (int val in values)
             {
-                // TODO: The continue statement skips the rest of the loop entirely
-                // and jumps to the next iteration (if there is one)
+            // TODO: The continue statement skips the rest of the loop entirely
+            // and jumps to the next iteration (if there is one)
+                // If value is > or = to 20 and val is < or = 29 ...
+                //      DONT'T execute anything else and instead move
+                //      immediately to the next iteration of the loop.
+                if (val >= 20 && val <= 29){
+                    continue;
+                }
 
 
                 // print the value
                 Console.WriteLine($"val is currently {val}");
 
-                // TODO: The break statement stops the loop and exits
+            // TODO: The break statement stops the loop and exits
+                // Stop loop from executing further if the currently
+                //  referenced value is 40
+                if (val >= 40) {
+                     break;
+                }
 
             }
         }
