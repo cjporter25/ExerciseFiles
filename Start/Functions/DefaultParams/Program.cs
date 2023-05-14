584c22dd043d6fe94ddd4bc4f1b3b463f09a2ee5
cjporter25 <chris.j.porter25@gmail.com>
2023-05-13 20:13:38 -0500

    Completed Default Parameters - 05_13_23

--- Start/Functions/DefaultParams/Program.cs
+++ Start/Functions/DefaultParams/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 
 // TODO: Functions can provide default values for their parameters
-void PrintWithPrefix(string thestr, string prefix)
-{
-    Console.WriteLine($"{prefix} {thestr}");
-}
+    // NOTE: By adding an "=" following a parameter name, you can
+    //      specify a default value to use if the function call didn't
+    //      include an input parameter at that index
+    void PrintWithPrefix(string thestr, string prefix="")
+    {
+        Console.WriteLine($"{prefix} {thestr}");
+    }
 
 // TODO: Test the default parameters
-
+    PrintWithPrefix("Hello There!");
+    PrintWithPrefix("Hello There!", ">: ");
 
 // TODO: Call with named params
-
+    // NOTE: By specifying a parameter's name before the item, separated by
+    //       a semi colon, the parameters can be out of their original order
+    PrintWithPrefix(prefix: "% ", thestr: "Hello There!");
