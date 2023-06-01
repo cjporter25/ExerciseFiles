9e2c2f2cc63dc2ceee1fb1b0770750a74cc38b93
cjporter25 <chris.j.porter25@gmail.com>
2023-05-31 21:00:01 -0500

    Completed Defining Classes - 05_31_23

--- Start/Classes/Defining/Book.cs
+++ Start/Classes/Defining/Book.cs
@@ -3,15 +3,28 @@ using System;
 namespace Defining
 {
     // classes have a name, unique within the namespace
+    // Usage:
+    //  1. Use the class keyword, followed by the preferred class name
+    //  2. Making a class public means it's accessible by any other code 
     public class Book
     {
-        // TODO: classes have member variables, or "fields" to hold data
+        // CLASSES have MEMBER variables, or "fields" to hold data
+        string _name;
+        string _author;
+        int _pagecount;
 
+        // CLASSES have one or more constructors, which are class methods
+        //      designed to create sed object
+        public Book(string name, string author, int pages) {
+            _name = name;
+            _author = author;
+            _pagecount = pages;
+        }
 
-        // TODO: classes have one or more constructors
-
-
-        // TODO: methods are used to operate on the class and data
+        // METHODS are used to operate on the class and data
+        public string GetDescription() {
+            return $"{_name} by {_author}";
+        }
 
     }
 }
--- Start/Classes/Defining/Program.cs
+++ Start/Classes/Defining/Program.cs
@@ -6,14 +6,16 @@ namespace Defining
     {
         static void Main(string[] args)
         {
-            // TODO: Create new object instances using the "new" operator
+        // Creating a new object instances using the "new" operator
+            Book b1 = new Book("War and Peace", "Leo Tolstoy", 825);
+            Book b2 = new Book("The Grapes of Wrath", "John Steinback", 464);
+        // Calling a method on an object
+            Console.WriteLine(b1.GetDescription());
+            Console.WriteLine(b2.GetDescription());
 
-            
-            // TODO: Call a method on the object
 
-
-            // TODO: try to set one of the properties -- 
-            // this will result in an error
+        // Try to set one of the properties -- this will result in an error
+            //b1._name = "Aldous Huxley";
 
         }
     }
